e6043d6dabc442135011be17e97f4b817cbf7d72
Alexander Klink <git@alech.de>
2018-08-29 09:22:30 +0200

    binarytree unfold

--- anamorphisms.hs
+++ anamorphisms.hs
@@ -13,3 +13,25 @@ myUnfoldr f b =
 betterIterate :: (a -> a) -> a -> [a]
 betterIterate f a =
     myUnfoldr (\a -> Just (a, f a)) a
+
+data BinaryTree a =
+      Leaf
+    | Node (BinaryTree a) a (BinaryTree a)
+    deriving (Eq, Ord, Show)
+
+unfold :: (a -> Maybe (a, b, a)) -> a -> BinaryTree b
+unfold f a =
+    case f a of
+        Nothing        -> Leaf
+        Just (l, v, r) -> Node (unfold f l) v (unfold f r)
+
+treeBuild :: Integer -> BinaryTree Integer
+treeBuild n =
+    unfold (buildLevels n) 0
+    where
+        buildLevels :: Integer -> Integer -> Maybe (Integer, Integer, Integer)
+        buildLevels n m =
+            if m >= n then
+                Nothing
+            else
+                Just (m + 1, m, m + 1)
