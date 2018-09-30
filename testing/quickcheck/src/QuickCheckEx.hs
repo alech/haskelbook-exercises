1d785e4a4ef12d821e96ec5e30ecc424c58baea3
Alexander Klink <git@alech.de>
2018-09-30 13:05:02 +0200

    chapter 14 chapter exercises 1-6

---
+++ testing/quickcheck/src/QuickCheckEx.hs
@@ -0,0 +1,131 @@
+module QuickCheckEx where
+
+import Test.QuickCheck
+import Data.List (sort)
+
+half :: Fractional a => a -> a
+half x = x / 2
+
+halfIdentity = (*2) . half
+
+prop_halfIdentity :: Property
+prop_halfIdentity =
+    forAll (arbitrary :: Gen Float) (\i -> halfIdentity i == i)
+
+listOrdered :: (Ord a) => [a] -> Bool
+listOrdered xs =
+    snd $ foldr go (Nothing, True) xs
+    where
+        go _ status@(_, False) = status
+        go y (Nothing, t)      = (Just y, t)
+        go y (Just x, t)       = (Just y, x >= y)
+
+prop_sortedListsAreOrdered :: Property
+prop_sortedListsAreOrdered =
+    forAll (arbitrary :: Gen [Int]) $ listOrdered . sort
+
+plusAssociative :: (Num a, Eq a) => a -> a -> a -> Bool
+plusAssociative x y z =
+    x + (y + z) == (x + y) + z
+
+plusCommutative :: (Num a, Eq a) => a -> a -> Bool
+plusCommutative x y =
+    x + y == y + x
+
+multAssociative :: (Num a, Eq a) => a -> a -> a -> Bool
+multAssociative x y z =
+    x * (y * z) == (x * y) * z
+
+multCommutative :: (Num a, Eq a) => a -> a -> Bool
+multCommutative x y =
+    x * y == y * x
+
+prop_associativity :: Property
+prop_associativity =
+    forAll (arbitrary :: Gen (Int, Int, Int))
+        (\(x, y, z) -> plusAssociative x y z)
+
+prop_commutativity :: Property
+prop_commutativity =
+    forAll (arbitrary :: Gen (Integer, Integer))
+        $ uncurry plusCommutative
+
+prop_multAssociativity :: Property
+prop_multAssociativity =
+    forAll (arbitrary :: Gen (Int, Int, Int))
+        (\(x, y, z) -> multAssociative x y z)
+
+prop_multCommutativity :: Property
+prop_multCommutativity =
+    forAll (arbitrary :: Gen (Integer, Integer))
+        $ uncurry multCommutative 
+
+quotRemLaw :: Integral a => a -> a -> Bool
+quotRemLaw x y =
+    quot x y * y + rem x y == x
+
+divModLaw :: Integral a => a -> a -> Bool
+divModLaw x y =
+    div x y * y + mod x y == x
+
+-- generate a pair of Integers where the second one is
+-- guaranteed to be non-0 (for use with div, quot)
+arbIntNonZeroInt :: Gen (Integer, Integer)
+arbIntNonZeroInt = do
+    a <- arbitrary :: Gen Integer
+    b <- arbitrary :: Gen Integer
+    if b == 0 then
+        return (a, 1)
+    else
+        return (a, b)
+
+prop_quotRemLaw :: Property
+prop_quotRemLaw =
+    forAll arbIntNonZeroInt $ uncurry quotRemLaw
+
+prop_divModLaw :: Property
+prop_divModLaw =
+    forAll arbIntNonZeroInt $ uncurry divModLaw
+
+powerAssociative :: Integral a => a -> a -> a -> Bool
+powerAssociative x y z =
+    x ^ (y ^ z) == (x ^ y) ^ z
+
+powerCommutative :: Integral a => a -> a -> Bool
+powerCommutative x y =
+    x ^ y == y ^ x
+    
+arbThreePositiveIntegers :: Gen (Integer, Integer, Integer)
+arbThreePositiveIntegers = do
+    a <- arbitrary :: Gen Integer
+    b <- arbitrary :: Gen Integer
+    c <- arbitrary :: Gen Integer
+    return (abs a, abs b, abs c)
+
+arbTwoPositiveIntegers :: Gen (Integer, Integer)
+arbTwoPositiveIntegers = do
+    a <- arbitrary :: Gen Integer
+    b <- arbitrary :: Gen Integer
+    return (abs a, abs b)
+
+prop_powerAssociative :: Property
+prop_powerAssociative =
+    forAll arbThreePositiveIntegers
+    (\(x, y, z) -> powerAssociative x y z)
+
+prop_powerCommutative :: Property
+prop_powerCommutative =
+    forAll arbTwoPositiveIntegers $ uncurry powerCommutative
+
+main :: IO ()
+main = do
+    quickCheck prop_halfIdentity
+    quickCheck prop_sortedListsAreOrdered
+    quickCheck prop_associativity
+    quickCheck prop_commutativity
+    quickCheck prop_multAssociativity
+    quickCheck prop_multCommutativity
+    quickCheck prop_quotRemLaw
+    quickCheck prop_divModLaw
+    quickCheck prop_powerAssociative
+    quickCheck prop_powerCommutative
