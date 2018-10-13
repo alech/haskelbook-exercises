237319606cc971d2b1cbd6e3bf097c5e8fe2dede
Alexander Klink <git@alech.de>
2018-10-13 23:23:05 +0200

    ch16: finished chapter exercises

--- ch16/src/Main.hs
+++ ch16/src/Main.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE FlexibleInstances #-}
+
 module Main where
 
 import Test.QuickCheck
@@ -95,6 +97,98 @@ data Sum a b =
 instance Functor (Sum a) where
     fmap _ (First a) = First a
     fmap f (Second b) = Second $ f b
+
+-- chapter exercises
+data Quant a b =
+      Finance
+    | Desk a
+    | Bloor b
+
+instance Functor (Quant a) where
+    fmap _ Finance   = Finance
+    fmap _ (Desk a)  = Desk a
+    fmap f (Bloor b) = Bloor $ f b
+
+data K a b =
+    K a
+    deriving (Eq, Show)
+
+instance Functor (K a) where
+    fmap _ (K a) = K a
+
+newtype Flip f a b =
+    Flip (f b a)
+    deriving (Eq, Show)
+
+instance Functor (Flip K a) where
+    fmap f (Flip (K b)) = Flip (K (f b))
+
+data EvilGoateeConst a b =
+    GoatyConst b
+    deriving (Eq, Show)
+
+instance Functor (EvilGoateeConst a) where
+    fmap f (GoatyConst b) = GoatyConst $ f b
+
+data LiftItOut f a =
+    LiftItOut (f a)
+    deriving (Eq, Show)
+
+instance Functor f => Functor (LiftItOut f) where
+    fmap g (LiftItOut x) = LiftItOut $ fmap g x
+
+data Parappa f g a =
+    DaWrappa (f a) (g a)
+    deriving (Eq, Show)
+
+instance (Functor f, Functor g) => Functor (Parappa f g) where
+    fmap h (DaWrappa x y) = DaWrappa (fmap h x) (fmap h y)
+
+data IgnoreOne f g a b =
+    IgnoreSomething (f a) (g b)
+    deriving (Eq, Show)
+
+instance (Functor g) => Functor (IgnoreOne f g a) where
+    fmap h (IgnoreSomething x y) = IgnoreSomething x (fmap h y)
+
+data Notorious g o a t =
+    Notorious (g o) (g a) (g t)
+    deriving (Eq, Show)
+
+instance (Functor g) => Functor (Notorious g o a) where
+    fmap h (Notorious x y z) = Notorious x y (fmap h z)
+
+data List a =
+      Nil
+    | Cons a (List a)
+    deriving (Eq, Show)
+
+instance Functor List where
+    fmap _ Nil = Nil
+    fmap f (Cons x xs) = Cons (f x) (fmap f xs)
+
+data GoatLord a =
+      NoGoat
+    | OneGoat a
+    | MoreGoats (GoatLord a)
+                (GoatLord a)
+                (GoatLord a)
+    deriving (Eq, Show)
+
+instance Functor GoatLord where
+    fmap _ NoGoat = NoGoat
+    fmap f (OneGoat a) = OneGoat $ f a
+    fmap f (MoreGoats x y z) = MoreGoats (fmap f x) (fmap f y) (fmap f z)
+
+data TalkToMe a =
+      Halt
+    | Print String a
+    | Read (String -> a)
+
+instance Functor TalkToMe where
+    fmap _ Halt        = Halt
+    fmap f (Print s a) = Print s (f a)
+    fmap f (Read g)    = Read $ f . g 
 
 main :: IO ()
 main = do
